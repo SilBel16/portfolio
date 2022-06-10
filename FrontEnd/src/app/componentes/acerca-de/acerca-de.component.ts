fb18524065a667faa1c276b2c0c43e90b830a30a
Silvina Belcastro <silvinabelcastro@gmail.com>
2022-06-10 16:02:36 -0300

    "Acerca de" utiliza datos PortfolioService // Script en app component.html

--- FrontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/service/portfolio.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
-
-  constructor() { }
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      console.log(data);
+      this.miPortfolio=data;
+    });
   }
 
 }
