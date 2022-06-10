3119576e0a49019d2fa2eb6baa2fb57f22cdf3aa
Silvina Belcastro <silvinabelcastro@gmail.com>
2022-06-10 15:32:19 -0300

    Instalación de paquetes, SQL y Wampserver

---
+++ BackEnd/src/test/java/com/portfolio/SilBel/SilBelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.SilBel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilBelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
