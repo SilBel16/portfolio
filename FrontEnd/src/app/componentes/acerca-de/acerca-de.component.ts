2863f38e4b01b1569e3cd828fd3a710e2c86cb1c
Silvina Belcastro <silvinabelcastro@gmail.com>
2022-08-12 16:46:32 -0300

    prueba unión Back con Front (acerca_de)

--- FrontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 import { PortfolioService } from 'src/app/service/portfolio.service';
 
 @Component({
@@ -7,14 +9,19 @@ import { PortfolioService } from 'src/app/service/portfolio.service';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
-  miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  /* miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { } */
+
+  persona: persona = new persona("", "", "");
+  constructor (public personaService:PersonaService) {}
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
+ /*   this.datosPortfolio.obtenerDatos().subscribe(data => {
       console.log(data);
       this.miPortfolio=data;
-    });
+    }); */
+
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
   }
 
 }
--- FrontEnd/src/app/service/persona.service.ts
+++ FrontEnd/src/app/service/persona.service.ts
@@ -3,21 +3,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { persona } from '../model/persona.model';
 
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class PersonaService {
 
-    //de donde quiero que llame al backend
-    URL = 'https://localhost8080/personas'
+  URL = 'http://localhost:8080/personas/'
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  public getPersona(): Observable<persona>{ //observable--> hacer peticiones asincronas
-    return this.http.get<persona>(this.URL+'/traer/perfil');
-  } 
-
-
-
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
+  }
 }
